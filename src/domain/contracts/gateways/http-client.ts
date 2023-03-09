3bf5b9bfffa554b6b4b9ee5e30bd11d92b09d2de
Elzio Nogueira Lima <elziolimach@gmail.com>
2023-03-08 20:26:06 -0400

    refactor: specialize http client contract

--- src/domain/contracts/gateways/http-client.ts
+++ src/domain/contracts/gateways/http-client.ts
@@ -1,15 +1,11 @@
 export interface HttpGetClient {
-  request: (input: HttpGetClient.Input) => Promise<HttpGetClient.Output>
+  get: (input: HttpGetClient.Input) => Promise<HttpGetClient.Output>
 }
 
 export namespace HttpGetClient {
   export type Input = {
-    baseURL: string
     url: string
-    method: string
-    body: any
     params: any
-    headers: any
   }
   
   export type Output = {
